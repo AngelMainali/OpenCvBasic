7acf8939981536d903f98ceab449f767020e6bde
AngelMainali <anjelmainali11@gmail.com>
2025-07-25 20:31:58 +0545

    2nd commit

--- opencv1.py
+++ opencv1.py
@@ -1,25 +1,45 @@
 import cv2 as cv
 
-img = cv.imread(r' ') #enter image path
+#img = cv.imread(r' ') #enter image path
 
-
-resized = cv.resize(img, (800, 600))
-flipped = cv.flip(resized, 0)
-gray=cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
-
-
-
+#resized = cv.resize(img, (800, 600))
+#flipped = cv.flip(resized, 0)
+#gray=cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
 
 #cv.rectangle(gray, (200, 200), (400, 400), (120,20,200), 7)
 
 #cv.imshow("Image", gray)
-
-
 #cv.imshow("img",img)
 
-edges=cv.Canny(resized,threshold1=10,threshold2=400)
+#edges=cv.Canny(resized,threshold1=10,threshold2=400)
 #edges=cv.Canny(resized,
-cv.imshow("Canny",edges)
+#cv.imshow("Canny",edges)
+
+
+# Load cascade
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+#pretained model for face detection
+# Read image
+img = cv.imread(r' ') #load image here
+resized = cv.resize(img, (800, 600))
+gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
+
+# Detect faces
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+# Draw rectangle around faces
+for (x, y, w, h) in faces:
+    cv.rectangle(resized, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+# Show output
+cv.imshow('Detected Face', resized)
+
+
+
+
+
+
 
 
 # Wait until a key is pressed
